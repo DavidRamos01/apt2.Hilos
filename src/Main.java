156ced63072e1cdf00d6aae76622bb60ab5b8402
David <david@gmail.com>
2025-11-23 12:06:09 +0100

    Implementar lógica principal de simulación de rescate en Main
    - Inicialización: Generar 352 pasajeros con prioridades aleatorias y cargarlos en el objeto Barco
    
    - Configuración: Instanciar las 5 balsas (Acasta, Banff, Cadiz, etc.) con sus capacidades y tiempos de rescate específicos.
    
    - Concurrencia: Crear y arrancar un Thread de rescate para cada balsa.
    
    - Sincronización Main: Implementar join() en los hilos para asegurar que el programa espere a que finalicen todos los rescates antes de imprimir el reporte final.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.ArrayList;
+import java.util.Random;
+
+//TIP To run code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        //Completar Codigo
 
+
+    public static void main(String[] args) throws InterruptedException {
+        int total_pasajeros = 352;
+
+        ArrayList<Pasajero> listaPasajeros = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 1; i <= total_pasajeros; i++) {
+            int prioridad = rand.nextInt(4) + 1;
+            listaPasajeros.add(new Pasajero(i, prioridad));
+        }
+        System.out.println("Se ha generado la tripulación de " + total_pasajeros + " personas.");
+
+        Barco laAlianza = new Barco(listaPasajeros);
+
+        Balsa acasta = new Balsa("Acasta", 1, 10);
+        Balsa banff = new Balsa("Banff", 2, 21);
+        Balsa cadiz = new Balsa("Cadiz", 3, 32);
+        Balsa deimos = new Balsa("Deimos", 4, 44);
+        Balsa expedicion = new Balsa("Expedición", 5, 80);
+
+        ArrayList<Balsa> balsas = new ArrayList<>();
+        balsas.add(acasta);
+        balsas.add(banff);
+        balsas.add(cadiz);
+        balsas.add(deimos);
+        balsas.add(expedicion);
+
+        ArrayList<Thread> hilosRescate = new ArrayList<>();
+        System.out.println("INICIANDO OPERACIÓN DE RESCATE CON 5 BALSAS");
+
+        for (Balsa balsa : balsas) {
+            Thread hilo = new Thread(new Rescate(laAlianza, balsa), "Hilo-" + balsa.getNombre());
+            hilosRescate.add(hilo);
+            hilo.start();
+        }
+
+        for (Thread hilo : hilosRescate) {
+            hilo.join();
+        }
+
+        System.out.println("\nOPERACIÓN DE RESCATE FINALIZADA");
+        System.out.println("Pasajeros restantes en el barco: " + laAlianza.getPasajerosRestantes());
     }
 }
