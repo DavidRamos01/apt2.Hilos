41fee8a622c005b79d96a32f1589db0c56847850
David <david@gmail.com>
2025-11-23 12:11:34 +0100

    Implementar lógica del rescate en clase Rescate
    - Vinculación: Añadir constructor para recibir las instancias compartidas de Barco y Balsa.
    
    - Bucle Principal: Implementar lógica en run() que extrae pasajeros del barco y los carga en la balsa continuamente.
    
    - Gestión de Viajes: Activar balsa.realizarViaje() automáticamente cuando la balsa alcanza su capacidad máxima.

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,8 +1,40 @@
-public class Rescate implements Runnable{
+public class Rescate implements Runnable {
 
-    public Rescate(){}
+    private Barco barco;
+    private Balsa balsa;
+
+    public Rescate(Barco barco, Balsa balsa) {
+        this.barco = barco;
+        this.balsa = balsa;
+    }
+
     @Override
     public void run() {
-        while(true){}
+        try {
+            while (barco.hayPasajeros() || balsa.getPasajerosABordo().size() > 0) {
+
+                Pasajero pasajero = barco.sacarSiguientePasajero();
+
+                if (pasajero != null) {
+                    boolean balsaLlena = balsa.cargarPasajero(pasajero);
+
+                    if (balsaLlena) {
+                        balsa.realizarViaje();
+                    }
+                } else {
+                    if (!barco.hayPasajeros()) {
+                        if (balsa.getPasajerosABordo().size() > 0) {
+                            balsa.realizarViaje();
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Rescate de la Balsa " + balsa.getNombre() + " interrumpido.");
+        } finally {
+            System.out.println("--- Balsa " + balsa.getNombre() + " ha finalizado su labor. ---");
+        }
     }
 }
